d8026e7de5758915a2f41d491fac64c0b9b232da
plxaq <sacha.glapa@gmail.com>
2025-04-03 17:40:11 +0200

    Adding last functionnalities and JSON server compatibility for API mockup

--- js/script.js
+++ js/script.js
@@ -1,141 +1,219 @@
 //This will load on page load
-document.addEventListener('DOMContentLoaded', function () {
-    // Load elements
-    const cards = document.querySelectorAll('.card');
-    const cardContainers = document.querySelector('.card-container');
-    const forms = document.querySelectorAll('.form-content');
-    fetch('/data/missions.json' , {
-        method: 'GET'
-    }).then(response => response.json()).then(data => {
-        console.log(data);
-        populateMissions(data);
-    }).catch(error => console.error('Error loading JSON:', error));
-    
-    // Function to handle card flip
-    function flipCard(card) {
-        const formContainer = card.querySelector('.form-container');
-        const formContent = card.querySelector('.form-content');
+document.addEventListener("DOMContentLoaded", function () {
+  // Load elements
+  const cards = document.querySelectorAll(".card");
+  const forms = document.querySelectorAll(".form-content");
+  const freelanceForm = document.getElementById("freelanceForm");
 
-        card.classList.toggle('flipped');
-
-
-        // Toggle form visibility based on flipped state
-        if (formContainer) {
-            setTimeout(() => {
-                formContainer.style.opacity = '1';
-                formContent.style.display = 'flex';
-            }, 300);
-        }
-
-        adjustCardHeight(card);
-        adjustFormWidth(card);
-    }
-
-    // Function to adjust card height dynamically
-    function adjustCardHeight(card) {
-        const formContent = card.querySelector('.form-content');
-        const cardBackMissions = card.querySelector('#cardBackMissions');
-
-        // Adjust FORM cards
-        if (card.classList.contains('flipped') && formContent) {
-            const formHeight = formContent.offsetHeight;
-            const cardHeight = card.offsetHeight;
-
-            if (formHeight > cardHeight) {
-                card.style.height = `${formHeight + (cardHeight/2)}px`;
-            }
-        } else {
-            card.style.height= '';
-        }
-
-        // Adjust Missions card
-        if (card.classList.contains('flipped') && cardBackMissions) {
-            const cardBackHeight = cardBackMissions.offsetHeight;
-            const cardHeight = card.offsetHeight;
-
-            if(cardBackHeight > cardHeight) {
-                card.style.height = `${cardBackHeight}px`
-            }
-        }
-    }
-    
-    // Function to adjust form elements to fit the size of the card
-    function adjustFormWidth(card) {
-        const form = card.querySelector('.form-content');
-        if (form) {
-            const cardBackWidth = card.querySelector('.card-back').offsetWidth;
-            form.style.width = `${cardBackWidth}px`;
-        }
-    }
-
-    function populateMissions(data) {
-        const cardBackMissions = document.querySelector('#cardBackMissions'); // Get the back of the "missions" card
-
-        // Clear any existing content in the back of the card to start fresh
-        cardBackMissions.innerHTML = '';
-    
-        // Create a title for the list of missions
-        const title = document.createElement('h3');
-        title.textContent = "Liste des missions disponibles";
-        cardBackMissions.appendChild(title);
-    
-        // Create a list to display all the mission data
-        const missionList = document.createElement('ul');
-    
-        // Loop through the data and create a list item for each mission
-        data.forEach(mission => {
-            const missionItem = document.createElement('li');
-            missionItem.innerHTML = `<strong>${mission.id}</strong>: ${mission.title} <br>Compétences requises: ${mission.skills}<br><br>`;
-            missionList.appendChild(missionItem);
-        });
-    
-        // Append the list to the back of the "missions" card
-        cardBackMissions.appendChild(missionList);
-        adjustCardHeight(cardBackMissions);
-    }
-
-    forms.forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            const name = document.querySelector('#name');
-            const email = document.querySelector('#email');
-            const missionID = document.querySelector('#missionID');
-
-            const freelanceData = {
-                name: name,
-                email: email,
-                missionID: missionID,
-            }
-
-            fetch('data/candidatures.json', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body : JSON.stringify(freelanceData),
-            }).then(response => response.json()).then(data => {
-                console.log('Success:', data);
-                alert("Votre candidature a bien été enregistrée!");
-            }).catch(error => {
-                console.log('Error:', error);
-                alert("Il y a eu une erreur lors de l'enregistrement de votre candidature.");
-            })
-        })
+  fetch("http://localhost:2777/missions")
+    .then((response) => response.json())
+    .then((data) => {
+      populateMissions(data);
+      selectMissions(data);
     })
+    .catch((error) => console.error("Error loading JSON:", error));
 
-    cards.forEach(card => {
-        card.addEventListener('click', function (event) {
-            if (!event.target.closest('.form-content')) {
-                flipCard(card);
-            }
-        });
+  // Function to handle card flip
+  function flipCard(card) {
+    const formContainer = card.querySelector(".form-container");
+    const formContent = card.querySelector(".form-content");
+
+    card.classList.toggle("flipped");
+
+    // Toggle form visibility based on flipped state
+    if (formContainer) {
+      setTimeout(() => {
+        formContainer.style.opacity = "1";
+        formContent.style.display = "flex";
+      }, 300);
+    }
+
+    adjustCardHeight(card);
+    adjustFormWidth(card);
+  }
+
+  // Function to fill forms Missions
+  function selectMissions(missions) {
+    const missionIDs = document.getElementById("missionID");
+    missionIDs.innerHTML = "";
+
+    missions.forEach((mission) => {
+      const id = document.createElement("option");
+      id.value = mission.id;
+      id.innerHTML = `<strong>${mission.id}. </strong>` + mission.title;
+      missionIDs.appendChild(id);
     });
-    
-    window.addEventListener('resize', function () {
-        cards.forEach(card => {
-            adjustCardHeight(card);
-            adjustFormWidth(card);
+  }
+
+  // Function to adjust card height dynamically
+  function adjustCardHeight(card) {
+    const formContent = card.querySelector(".form-content");
+    const cardBackMissions = card.querySelector("#cardBackMissions");
+
+    // Adjust FORM cards
+    if (card.classList.contains("flipped") && formContent) {
+      const formHeight = formContent.offsetHeight;
+      const cardHeight = card.offsetHeight;
+
+      if (formHeight > cardHeight) {
+        card.style.height = `${formHeight + cardHeight / 2}px`;
+      }
+    } else {
+      card.style.height = "";
+    }
+
+    // Adjust Missions card
+    if (card.classList.contains("flipped") && cardBackMissions) {
+      const cardBackHeight = cardBackMissions.offsetHeight;
+      const cardHeight = card.offsetHeight;
+
+      if (cardBackHeight > cardHeight) {
+        card.style.height = `${cardBackHeight}px`;
+      }
+    }
+  }
+
+  // Function to adjust form elements to fit the size of the card
+  function adjustFormWidth(card) {
+    const form = card.querySelector(".form-content");
+    if (form) {
+      const cardBackWidth = card.querySelector(".card-back").offsetWidth;
+      form.style.width = `${cardBackWidth}px`;
+    }
+  }
+
+  function populateMissions(data) {
+    const cardBackMissions = document.querySelector("#cardBackMissions"); // Get the back of the "missions" card
+
+    // Clear any existing content in the back of the card to start fresh
+    cardBackMissions.innerHTML = "";
+
+    // Create a title for the list of missions
+    const title = document.createElement("h3");
+    title.textContent = "Liste des missions disponibles";
+    cardBackMissions.appendChild(title);
+
+    // Create a list to display all the mission data
+    const missionList = document.createElement("ul");
+
+    // Loop through the data and create a list item for each mission
+    data.forEach((mission) => {
+      const missionItem = document.createElement("li");
+      missionItem.innerHTML = `<strong>${mission.id}</strong>: ${mission.title} <br>Compétences requises: ${mission.skills}<br><br>`;
+      missionList.appendChild(missionItem);
+    });
+
+    // Append the list to the back of the "missions" card
+    cardBackMissions.appendChild(missionList);
+    adjustCardHeight(cardBackMissions);
+  }
+
+  // Load candidatures
+  function loadCandidatures() {
+    const cardBackDashboard = document.querySelector("#cardBackDashboard");
+
+    cardBackDashboard.innerHTML = "";
+
+    const title = document.createElement("h3");
+    title.innerHTML = `Dashboard<br><br>`;
+    cardBackDashboard.appendChild(title);
+
+    const dashboardList = document.createElement("ul");
+
+    fetch("http://localhost:2777/candidatures")
+      .then((response) => response.json())
+      .then((candidatures) => {
+        candidatures.forEach((candidature) => {
+          const dashboardItem = document.createElement("li");
+          dashboardItem.innerHTML = `<strong>${candidature.name}</strong>`;
+
+
+          fetch(`http://localhost:2777/missions/${candidature.missionID}`)
+            .then((response) => response.json())
+            .then((mission) => {
+              const missionData = document.createElement("li");
+              missionData.innerHTML = `${mission.title}`;
+              dashboardList.appendChild(dashboardItem);
+              dashboardList.appendChild(missionData);
+              const delLign = document.createElement("li");
+              const delButton = document.createElement("button");
+              delButton.textContent = "Supprimer";
+              delButton.addEventListener('click', function(event) {
+                event.stopPropagation();
+                deleteCandidature(candidature.id);
+              });
+              delLign.appendChild(delButton);
+              dashboardList.appendChild(delLign);
+              const carriageReturn = document.createElement("li");
+              carriageReturn.innerHTML = "<br><br>";
+              dashboardList.appendChild(carriageReturn);
+            }).catch(error => console.error('Error loading mission data:', error));
+
+        });
+      }).catch(error => console.error('Error loading candidatures:', error));
+
+      cardBackDashboard.appendChild(dashboardList);
+      adjustCardHeight(cardBackDashboard);
+  }
+
+  function deleteCandidature(id) {
+    fetch(`http://localhost:2777/candidatures/${id}`, {
+        method: 'DELETE',
+    }).then(() => {
+      alert("Candidature supprimée");
+      loadCandidatures();
+    }).catch(error => console.error("Erreur lors de la suppression:", error));
+  }
+
+  freelanceForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const missionID = document.querySelector("#missionID").value;
+
+    const candidature = {
+      name: name,
+      email: email,
+      missionID: missionID,
+    };
+
+    // This would be the way to handle POST request but browsers cannot write on local files.
+    if (candidature.missionID) {
+      fetch("http://localhost:2777/candidatures", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(candidature),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          alert("Votre candidature a bien été enregistrée!");
         })
-    })
+        .catch((error) => {
+          console.log("Error:", error);
+          alert(
+            "Il y a eu une erreur lors de l'enregistrement de votre candidature."
+          );
+        });
+    }
+  });
+
+  cards.forEach((card) => {
+    card.addEventListener("click", function (event) {
+      if (!event.target.closest(".form-content")) {
+        flipCard(card);
+      }
+    });
+  });
+
+  window.addEventListener("resize", function () {
+    cards.forEach((card) => {
+      adjustCardHeight(card);
+      adjustFormWidth(card);
+    });
+  });
+  loadCandidatures();
 });
